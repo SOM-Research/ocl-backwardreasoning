8012c633156dfba0392310090182f0b4ccc263f7
hamza <hamza.eddouibi@gmail.com>
2015-06-23 19:39:09 +0200

    complete production system example

--- examples/cat.icrea.ocl.backwardreasoning.example.productionsystem/src/cat/icrea/ocl/backwardreasoning/example/productionsystem/UsingBackwardReasoning.java
+++ examples/cat.icrea.ocl.backwardreasoning.example.productionsystem/src/cat/icrea/ocl/backwardreasoning/example/productionsystem/UsingBackwardReasoning.java
@@ -1,11 +1,8 @@
 package cat.icrea.ocl.backwardreasoning.example.productionsystem;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EAttribute;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -14,15 +11,17 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 import ProductionSystem.Conveyor;
+import ProductionSystem.Processed;
+import ProductionSystem.ProductionSystemFactory;
 import ProductionSystem.ProductionSystemPackage;
+import ProductionSystem.Raw;
 import cat.icrea.ocl.backwardreasoning.facade.OCLBackWardReasoning;
 import cat.icrea.ocl.backwardreasoning.utils.OCLUtil;
 
-import org.eclipse.ocl.common.OCLCommon;
 import org.eclipse.ocl.ecore.Constraint;
 
 public class UsingBackwardReasoning {
-	public static String XMI_FILE = "model/machine.xmi";
+	public static String XMI_FILE = "model/Machine.xmi";
 
 	public static void main(String[] args) throws Exception {
 
@@ -45,26 +44,38 @@ public class UsingBackwardReasoning {
 		// Load the bank model
 		Resource resource = loadModel();
 		// Load the OCL constraints
-		List<Constraint> constraints = OCLUtil.parseOCL(
-				URI.createFileURI("model/machine.ocl"), ProductionSystemPackage.eINSTANCE);
+		Constraint constraint = OCLUtil.parseOCL(
+				URI.createFileURI("model/productionSystem.ocl"), ProductionSystemPackage.eINSTANCE).get(0);
 		// Test classes
 		Conveyor co1 = (Conveyor) EcoreUtil.getEObject(resource.getContents()
 				.get(0), "@ic.0");
+		Raw raw1 = (Raw) EcoreUtil.getEObject(resource.getContents()
+				.get(0), "@ic.0/@piece.0");
+		ProductionSystemFactory factory = ProductionSystemFactory.eINSTANCE;
+		Processed processed = factory.createProcessed();
 	
-		System.out.println("List of constraints");
-		System.out.println(constraints);
+		System.out.println("Constraint");
+		System.out.println(constraint);
 		System.err
 				.println("Delete the object r");
 		OCLBackWardReasoning oclBackWardReasoning = new OCLBackWardReasoning();
-		for (Constraint c : constraints) {
-			System.out.println("Post-condition : " + c);
-			Constraint temp = oclBackWardReasoning.deleteObjet(c,
-					co1.eClass(), "r");
+		
+			System.out.println("Post-condition : " + constraint);
+			Constraint temp = oclBackWardReasoning.deleteObjet(constraint,
+					raw1.eClass(), "r");
 			System.out.println("Pre-condition : "
 					+ temp.getSpecification().getBodyExpression());
-			System.out.println("");
-			
-		}
+			System.err
+			.println("creating a link between co1 and p");
+			temp = oclBackWardReasoning.createLink(temp, co1.eClass(), processed.eClass(), "co1", "p");
+			System.out.println("Pre-condition : "
+					+ temp.getSpecification().getBodyExpression());
+		
+			System.err
+			.println("creating object p");
+			temp = oclBackWardReasoning.createObject(temp, processed.eClass(), "p");
+			System.out.println("Pre-condition : "
+					+ temp.getSpecification().getBodyExpression());
 		
 		
 		
