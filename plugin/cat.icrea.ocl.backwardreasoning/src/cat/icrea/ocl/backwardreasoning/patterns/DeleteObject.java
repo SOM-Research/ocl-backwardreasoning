66c7b792f2f595ddc8dcee309c42d09e73cf2ae5
hamza <hamza.eddouibi@gmail.com>
2015-07-01 17:13:33 +0200

    improve pattern recognition for deleting an object

--- plugin/cat.icrea.ocl.backwardreasoning/src/cat/icrea/ocl/backwardreasoning/patterns/DeleteObject.java
+++ plugin/cat.icrea.ocl.backwardreasoning/src/cat/icrea/ocl/backwardreasoning/patterns/DeleteObject.java
@@ -14,6 +14,7 @@ import org.eclipse.ocl.ecore.Constraint;
 import org.eclipse.ocl.ecore.EcoreFactory;
 import org.eclipse.ocl.ecore.EcorePackage;
 import org.eclipse.ocl.ecore.OCL;
+import org.eclipse.ocl.ecore.SetType;
 import org.eclipse.ocl.ecore.internal.OCLStandardLibraryImpl;
 import org.eclipse.ocl.expressions.OCLExpression;
 import org.eclipse.ocl.expressions.OperationCallExp;
@@ -24,6 +25,7 @@ import org.eclipse.ocl.helper.OCLHelper;
 import org.eclipse.ocl.utilities.ExpressionInOCL;
 
 import cat.icrea.ocl.backwardreasoning.utils.OCLUtil;
+import cat.icrea.ocl.backwardreasoning.visitors.LookupAllInstancesVisitor;
 import cat.icrea.ocl.backwardreasoning.visitors.LookupPropertyVisitor;
 
 
@@ -55,10 +57,11 @@ public class DeleteObject {
 
 		OCLExpression bodyExp = oclExpression.getBodyExpression();
 		LookupPropertyVisitor lookupVisitor = new LookupPropertyVisitor();
+		LookupAllInstancesVisitor lookupAllInstancesVisitor = new LookupAllInstancesVisitor();
 		bodyExp.accept(lookupVisitor);
 		Set<PropertyCallExp<EClassifier, EStructuralFeature>> result = lookupVisitor.getResult();
 		for(PropertyCallExp<EClassifier, EStructuralFeature> item : result)
-			if (item.getReferredProperty().getEType() instanceof EClass && ((EClass)item.getReferredProperty().getEType()).isSuperTypeOf(eClass)) {
+			if ((item.getReferredProperty().getEType() instanceof EClass && ((EClass)item.getReferredProperty().getEType()).isSuperTypeOf(eClass)) || item.getReferredProperty().getEType() instanceof EClass && eClass.isSuperTypeOf((EClass)item.getReferredProperty().getEType())) {
 		
 			EcorePackage oclPackage = (EcorePackage) oclExpression.eClass()
 					.getEPackage();
@@ -81,8 +84,39 @@ public class DeleteObject {
 			operationCallExp.setSource(item);
 			
 			op.setSource(operationCallExp);
+			
+			
 
 		}
+		bodyExp.accept(lookupAllInstancesVisitor);
+		Set<OperationCallExp<EClassifier, EOperation>> resultAllInstances = lookupAllInstancesVisitor.getResult();
+		
+		
+		for(OperationCallExp<EClassifier, EOperation> item : resultAllInstances)
+			if ((item.getType() != null &&  item.getType() instanceof SetType && ((SetType)item.getType()).getElementType() instanceof EClass && ((EClass)((SetType)item.getType()).getElementType()).isSuperTypeOf(eClass)) || (item.getType() != null &&  item.getType() instanceof SetType && ((SetType)item.getType()).getElementType() instanceof EClass && eClass.isSuperTypeOf(((EClass)((SetType)item.getType()).getElementType())))) {
+				
+			EcorePackage oclPackage = (EcorePackage) oclExpression.eClass()
+					.getEPackage();
+			EcoreFactory oclFactory = (EcoreFactory) oclPackage
+					.getEFactoryInstance();
+			org.eclipse.ocl.ecore.IteratorExp op = (org.eclipse.ocl.ecore.IteratorExp) item.eContainer();
+			VariableExp variable = oclFactory.createVariableExp();
+			variable.setName(variableName);
+			variable.setType(eClass);
+			Variable v = oclFactory.createVariable();
+			v.setName(variableName);
+			v.setType(eClass);
+			variable.setReferredVariable(v);
+			OperationCallExp operationCallExp = oclFactory.createOperationCallExp();
+			EOperation including = org.eclipse.emf.ecore.EcoreFactory.eINSTANCE.createEOperation();
+			including.setName("excluding");
+			operationCallExp.setReferredOperation(including);
+			operationCallExp.getArgument().add(variable);
+			operationCallExp.setSource(item);
+			op.setSource(operationCallExp);
+
+		}
+		
 	}
 	public Constraint getConstraint() {
 		return constraint;
